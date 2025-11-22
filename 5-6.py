d8e8306ecdea2f3808d0de7d57a19f181ca9ae48
bartli555 <barlis3456@gmail.com>
2025-11-22 13:41:28 +0100

    Update 5-6.py

--- 5-6.py
+++ 5-6.py
@@ -1,4 +1,4 @@
-###
+'''###
 # Calculates the sum of even numbers from 1 to a given number N
 #
 N = 10
@@ -9,4 +9,16 @@ for i in range(1, N + 1):
     if i % 2 == 0:
         sum_even += i
 
-print(f"The sum of even numbers from 1 to {N} is: {sum_even}")
+print(f"The sum of even numbers from 1 to {N} is: {sum_even}")'''
+
+N = 10
+sum_even = 0
+
+i = 1
+
+while i <=N:
+    if i % 2 == 0:
+        sum_even += i
+    i += 1
+
+    print(f"The sum of even numbers from 1 to {N} is: {sum_even}")    
